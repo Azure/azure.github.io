4f0efe434ec0b9e93eb5dd185d5fd63c438abc17
David Justice <david@devigned.com>
2016-03-25 15:09:53 -0700

    working authentication via aad

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,36 +1,24 @@
-$(document).ready(function() {
-  var idValue = window.location.pathname.replace(/\//g, '');
-  if(!idValue) {
-    idValue = ["home"];
-  } else {
-    idValue = [idValue];
-  }
+$(document).ready(function () {
+    var idValue = window.location.pathname.replace(/\//g, '');
+    if (!idValue) {
+        idValue = ["home"];
+    } else {
+        idValue = [idValue];
+    }
 
-  if(idValue[0].match(/projects/)){
-    idValue.push("projects");
-  }
+    if (idValue[0].match(/projects/)) {
+        idValue.push("projects");
+    }
 
-  $(".nav").find(".active").removeClass("active");
-  _.each(idValue, function(i){
-    $("#" + i + 'Link').addClass("active");
-  });
-
-  // ['azure-sdk-for-net', 'azure-storage-net'].forEach(function(repo) {
-  //   Github.repoProfile({ username: 'azure', reponame: repo, selector: '.' + repo});
-  // });
-
+    $(".nav").find(".active").removeClass("active");
+    _.each(idValue, function (i) {
+        $("#" + i + 'Link').addClass("active");
+    });
 
 });
 
-// Github.gitApiUrl="http://localhost:4000/proxy/api.github.com/";
-
-
-// Github.orgProfile({
-//   orgname: 'azure',
-//   selector: '.azure-org'
-// });
-
-// Github.orgActivity({
-//  orgname: 'azure',
-//  selector: '.azure-org-activity'
-// });
+function log() {
+    if ('console' in window) {
+        console.log.apply(console, arguments);
+    }
+}
